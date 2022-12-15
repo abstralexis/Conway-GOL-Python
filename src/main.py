fa5b98db0a087a8f5ae8e11ce519345b774a35bc
AlexisComix <Thecomixguy@hotmail.com>
2022-12-15 13:31:00 +0000

    Comment and clean up code

--- src/main.py
+++ src/main.py
@@ -15,32 +15,47 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Life") 
 pygame.init
 
-def main() -> None:   
-    array: np.ndarray = new_array()
+def main() -> None:
+    """
+    Main function for the Game of Life
+    """
+    # Read the seed file to a new array
+    array: np.ndarray = new_array()     
     array = read_seed()
+
+    # Main loop
     running = True
     generations = 0
     while running:
-        handle_events()
-        array = process_matrix(array)
-        draw(array)
+        handle_events()                 # Handle inputs
+        array = process_matrix(array)   # Process array to new array
+        draw(array)                     # Draw array
 
+        # Increase generations count once per frame and set title
         generations += 1
         pygame.display.set_caption(f"Life | Generations: {generations}") 
 
-        CLOCK.tick(12)
+        CLOCK.tick(12)  # 12 fps
         
 def handle_events() -> None:
+    """
+    Handles the input events
+    """
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            # Close process on window close
             pygame.quit()
             sys.exit()
             
 def draw(matrix: np.ndarray) -> None:
+    """
+    Takes in the board matrix and draws it to the screen
+
+    #TODO change hard coded values
+    """
     WIN.fill(WHITE)
-    i = 0
-    j = 0
-    for i, row in enumerate(matrix):
+    # Iterate through matrix and row to draw live cells to grid
+    for i, row in enumerate(matrix):                
         y = i * 5
         for j, col in enumerate(row):
             x = j * 5
@@ -51,39 +66,37 @@ def draw(matrix: np.ndarray) -> None:
     pygame.display.flip()
     
 def new_array() -> np.ndarray:
+    """
+    Returns a new (100, 100) ndarray
+    """
     return np.zeros((100, 100))
 
-def process_tile(matrix: np.ndarray, i: int, j: int) -> None:
+def process_matrix(matrix: np.ndarray) -> np.ndarray:
     """
-    Modifies a tile in a matrix of of 0s and 1s with the rules:
+    Returns a new matrix of of 0s and 1s based 
+    off of argument matrix with the rules:
 
     If alive,
-        if number of neigbours == 2 or 3
+        if number of neighbours == 2 or 3
             stay alive
         else
             die
     if dead:
-        if num neigbours == 3
+        if num neighbours == 3
             live
     """
-    neighbours = get_neighbours(matrix, i, j)
-    neighbours = neighbours.count(1)
-    if matrix[i][j] == 1:
-        if neighbours == 2 or neighbours == 3:
-            matrix[i][j] = 1
-        else:
-            matrix[i][j] = 0
-    else:
-        if neighbours == 3:
-            matrix[i][j] = 1
-
-def process_matrix(matrix: np.ndarray) -> np.ndarray:
+    # Get new matrix to return as copy
     new = matrix.copy()
 
+    # Iterate through new matrix then rows and update new
+    # matrix based off of argument matrix
     for i, row in enumerate(new):
         for j, _ in enumerate(row):
+            # Get neighbours and count number of live
             neighbours = get_neighbours(matrix, i, j)
             neighbours = neighbours.count(1)
+
+            # Process the current tile with the rules
             if matrix[i][j] == 1:
                 if neighbours == 2 or neighbours == 3:
                     new[i][j] = 1
@@ -100,7 +113,7 @@ def get_neighbours(array: np.ndarray, i: int, j :int) -> list:
     Gets list of the 8 neighbouring values to array[i][j].
     Assumes C-Style (row-major) ndarray.
     
-    Does not return any neigbours for edge tiles.
+    Does not return any neigbours for edge tiles. #TODO Fix
     """
     height_index = array.shape[0] - 1
     width_index = array.shape[1] - 1
@@ -121,22 +134,15 @@ def get_neighbours(array: np.ndarray, i: int, j :int) -> list:
         array[i+1][j-1],
         array[i][j-1]
     ]
-    
-# def seed_matrix(matrix: np.ndarray):
-#     try:
-#         matrix[5][5] = 1
-#         matrix[5][6] = 1
-#         matrix[6][6] = 1
-#         matrix[6][5] = 1
-        
-#         matrix[20][20] = 1
-#         matrix[21][20] = 1
-#         matrix[22][20] = 1
-#     finally:
-#         pass
 
 def read_seed() -> np.ndarray:
-    seed_matrix = []
+    """
+    Reads seed from seed.txt and returns ndarray
+
+    Opens file and parses for a (100, 100 ndarray)
+    TODO fix hard-coding
+    """
+    seed_matrix: list = []
     with open("seed.txt", "r") as seedfile:
         seed = seedfile.read().split(",")
         for i in range(100):
--- src/seed_generator.py
+++ src/seed_generator.py
@@ -15,31 +15,43 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Life seed generator") 
 pygame.init()
 
-def main() -> None:   
-    array: np.ndarray = new_array()
-    running = True
+def main() -> None:
+    """
+    Main function for seed creation program
+    """
+    array: np.ndarray = new_array()         # Gets a new array
+    # Main loop handles events and draws array at 60 fps
+    running = True                          
     while running:
-        handle_events(array)
+        handle_events(array)                
         draw(array) 
         CLOCK.tick(60)
         
 def handle_events(matrix: np.ndarray) -> None:
+    """
+    Handles window events e.g inputs
+    """
     for event in pygame.event.get():
+        # Exit process on window close
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        # Mouse button events
         elif event.type == pygame.MOUSEBUTTONDOWN:
+            # Left click gets current tile and makes it live
             if event.button == 1:
                 x, y = pygame.mouse.get_pos()
                 i = y // 5
                 j = x // 5
                 matrix[i][j] = 1
+            # Right click gets current tile and makes it dead
             elif event.button == 3:
                 x, y = pygame.mouse.get_pos()
                 i = y // 5
                 j = x // 5
                 matrix[i][j] = 0
     
+    # If Return/Enter pressed, save array to seed.txt
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RETURN]:
         with open("seed.txt", "w") as seedfile:
@@ -52,9 +64,10 @@ def handle_events(matrix: np.ndarray) -> None:
         print("Write successful")
             
 def draw(matrix: np.ndarray) -> None:
+    """
+    Draws the matrix to the screen
+    """
     WIN.fill(WHITE)
-    i = 0
-    j = 0
     for i, row in enumerate(matrix):
         y = i * 5
         for j, col in enumerate(row):
@@ -66,6 +79,10 @@ def draw(matrix: np.ndarray) -> None:
     pygame.display.flip()
     
 def new_array() -> np.ndarray:
+    """
+    Gets new (100, 100) ndarray
+    # TODO fix hard-coding
+    """
     return np.zeros((100, 100))
         
 if __name__ == "__main__":

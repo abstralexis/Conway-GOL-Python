672c027a862c46bb6874a080493de0301c2c416f
AlexisComix <thecomixguy@hotmail.com>
2023-01-05 09:35:18 +0000

    Update to use click and drag with modes

--- src/seed_generator.py
+++ src/seed_generator.py
@@ -6,11 +6,16 @@ import pygame
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+GREY = (128, 128, 128)
 
 WIDTH = 500
 HEIGHT = 500
 CLOCK = pygame.time.Clock()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.font.init()
+SYSFONT = pygame.font.SysFont(pygame.font.get_default_font(), 18)
+mode = "draw"
 
 pygame.display.set_caption("Life seed generator") 
 pygame.init()
@@ -31,6 +36,7 @@ def handle_events(matrix: np.ndarray) -> None:
     """
     Handles window events e.g inputs
     """
+    global mode
     for event in pygame.event.get():
         # Exit process on window close
         if event.type == pygame.QUIT:
@@ -38,18 +44,9 @@ def handle_events(matrix: np.ndarray) -> None:
             sys.exit()
         # Mouse button events
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            # Left click gets current tile and makes it live
-            if event.button == 1:
-                x, y = pygame.mouse.get_pos()
-                i = y // 5
-                j = x // 5
-                matrix[i][j] = 1
-            # Right click gets current tile and makes it dead
-            elif event.button == 3:
-                x, y = pygame.mouse.get_pos()
-                i = y // 5
-                j = x // 5
-                matrix[i][j] = 0
+            # Right click changes mode
+            if event.button == 3:
+                mode = "erase" if mode == "draw" else "draw"
     
     # If Return/Enter pressed, save array to seed.txt
     keys = pygame.key.get_pressed()
@@ -62,11 +59,20 @@ def handle_events(matrix: np.ndarray) -> None:
 
             seedfile.write(seed)
         print("Write successful")
+        
+    # Click and drag
+    mousebuttons = pygame.mouse.get_pressed()
+    if mousebuttons[0]: # Left mouse button
+        x, y = pygame.mouse.get_pos()
+        i = y // 5
+        j = x // 5
+        matrix[i][j] = 1 if mode == "draw" else 0
             
 def draw(matrix: np.ndarray) -> None:
     """
     Draws the matrix to the screen
     """
+    global mode
     WIN.fill(WHITE)
     for i, row in enumerate(matrix):
         y = i * 5
@@ -76,7 +82,13 @@ def draw(matrix: np.ndarray) -> None:
                 rect = pygame.rect.Rect(x, y, 5, 5)
                 pygame.draw.rect(WIN, BLACK, rect)
 
+    draw_mode_text(mode)
     pygame.display.flip()
+    
+def draw_mode_text(mode):
+    text = "Draw" if mode == "draw" else "Erase"
+    mode_text = SYSFONT.render(text, True, GREY)
+    WIN.blit(mode_text, (5, 5))
     
 def new_array() -> np.ndarray:
     """
